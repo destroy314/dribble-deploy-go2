78ed4e997fa5217a01984e8686894b72cc1709f3
Mingrui-Yu <ymr20@mails.tsinghua.edu.cn>
2024-11-03 21:08:01 +0800

    update

---
+++ scripts/gait_visualize.py
@@ -0,0 +1,13 @@
+# TODO: to be implemented
+from matplotlib import pyplot as plt
+
+def plot_joint_pos():
+    fig, axs = plt.subplots(2, 2, figsize=(12, 5))
+
+
+
+
+
+
+if __name__ == "__main__":
+    plot_joint_pos()
