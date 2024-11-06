1609e9e72ef1cfecbae4317fe13f9dece413fcb8
xander <1073692867@qq.com>
2024-11-06 21:03:02 +0800

    update

--- deploy.py
+++ deploy.py
@@ -48,7 +48,7 @@ th_clip_actions_high = torch.tensor(clip_actions_high, device=device)
 th_clip_actions_low = torch.tensor(clip_actions_low, device=device)
 
 
-def load_policy(root: Path, model_name: str = "go2_friction", device="cuda:0"):
+def load_policy(root: Path, model_name: str = "go2_friction", device="cpu"):
     body_path = glob.glob(os.path.join(root, "models", model_name, "body*"))[0]
     adaptation_module_path = glob.glob(
         os.path.join(root, "models", model_name, "adaptation_module*")
@@ -60,15 +60,8 @@ def load_policy(root: Path, model_name: str = "go2_friction", device="cuda:0"):
     @torch.no_grad()
     def policy(stacked_history: torch.Tensor):
         # stacked_history: (H, d) = (15, 75)
-
-        # history = stacked_history.reshape(1, 1125)
-        # latent = adaptation_module(history)  # (1, 6)
-        # composed = torch.cat((history, latent), dim=-1)
-        # action = body(composed)
-        # return action[0]
-
-        obs_history = stacked_history.reshape(1, 1125).squeeze().to(device)
-        latent = adaptation_module.forward(obs_history)
+        obs_history = stacked_history.reshape(1, 1125).squeeze().to(device)  # (1125, )
+        latent = adaptation_module.forward(obs_history)  # (6, )
         action = body.forward(torch.cat((obs_history, latent), dim=-1))
         return action
 
@@ -117,13 +110,14 @@ class DribbleEnv:
 
         self.robot = robot
 
-        self.ball_position = [0.27402842, -0.04910268, 0.0]
+        self.ball_position = [0.27402842, -0.04910268, 0.0]   # TODO: init with zero position
         self.ball_subscriber = BallSubscriber(self)
 
     def observe(self):
         robot_obs = self.robot.get_obs()
         obs = self.make_obs(robot_obs)
         self.store_obs(obs)
+        print(f"self.t: {self.t}")
         return self.buffer[self.t - self.history_len : self.t], robot_obs
 
     def store_obs(self, obs: torch.Tensor):
@@ -149,7 +143,7 @@ class DribbleEnv:
     def make_obs(self, robot_obs: RobotObservation) -> torch.Tensor:
         ball_pos = torch.tensor(self.ball_position, device=device)
         projected_gravity = project_gravity(robot_obs.quaternion)
-        commands = [    # TODO: setting the smaller commands to zero
+        commands = [  # TODO: setting the smaller commands to zero
             # rocker x: left/right
             # rocker y: forward/backward
             robot_obs.ly * 2.0,  # x vel  # TODO: ?
@@ -239,11 +233,11 @@ def save_observation_to_file(obs, filename="observation_output.txt", mode="a"):
         file.write("-" * 40 + "\n")
 
 
-def main():
+def main(args):
     policy = load_policy(
-        Path(__file__).resolve().parent, model_name="go2_friction", device=device
+        Path(__file__).resolve().parent, model_name=args.model_name, device=device
     )
-    
+
     rclpy.init()
     robot = Robot()
     env = DribbleEnv(history_len=15, robot=robot)
@@ -268,46 +262,49 @@ def main():
         if robot_obs.L1:
             break
         time.sleep(env.dt)
-        
+
     print("Robot started, press L2 to stop")
-    
+
     env.yaw_init = robot_obs.yaw
     env.buffer[:, 73] = 0.0  # clear yaw sensor
 
     robot.to_run()
-    
-    benchmark = True
-    log = False
+
+    benchmark = args.benchmark
+    log = args.log
     assert not (benchmark and log), "Cannot benchmark and log at the same time"
     if log:
         i = 0
         all_obs = []
         all_actions = []
-    
+
     while True:
         if benchmark:
             begin = time.perf_counter()
-        
+
         obs, robot_obs = env.observe()
         action = policy(obs)
         env.advance(action)
         if robot_obs.L2:
             break
-        
+
         if benchmark:
             end = time.perf_counter()
             print(end - begin)
-            time.sleep(max(0, begin + env.dt - end))
-        
+            # time.sleep(max(0, begin + env.dt - end))
+            
         if log:
             all_obs.append(obs[14, 21:33].cpu().numpy())
             all_actions.append(obs[14, 45:57].cpu().numpy())
-            
+
             i += 1
             if i == 1:
                 save_observation_to_file(obs[14], mode="w")
             else:
                 save_observation_to_file(obs[14], mode="a")
+        
+        while time.perf_counter() < begin + env.dt:
+            pass
 
     if log:
         all_obs = np.array(all_obs)
@@ -329,4 +326,13 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--model_name", type=str, default="go2_friction_2")
+    parser.add_argument("--log", action="store_true", default=False)
+    parser.add_argument("--benchmark", action="store_true", default=False)
+
+    args = parser.parse_args()
+    main(args)
